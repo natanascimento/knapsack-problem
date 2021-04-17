3d263388babe1365b95538463c68c33cc35c4284
Natan Nascimento O Matos <natan@teltecsolutions.com.br>
2021-04-16 22:48:16 -0300

    Definindo construtor do problema

---
+++ KnapsackProblem/KnapsackProblem.py
@@ -0,0 +1,17 @@
+import random
+from deap import algorithms as alg, base, creator, tools
+
+class KnapsackProblem:
+  
+  def __init__(self, max_weight, length_items, max_weight_items, max_price_items, lenght_population, lenght_generations):
+    self.max_weight = max_weight
+    self.length_items = length_items
+    self.max_weight_items = max_weight_items
+    self.max_price_items = max_price_items
+    self.lenght_population = lenght_population
+    self.lenght_generations = lenght_generations
+    self.items = []
+    self.population = []
+    self.toolbox = base.Toolbox()
+    self.best_option = 0
+    
